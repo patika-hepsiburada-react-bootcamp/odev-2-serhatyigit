92ef8e1207f0d8dd86253252c1324faafee169e4
serhatyigit <83660988+serhatyigit@users.noreply.github.com>
2021-10-04 00:53:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# odev-2-serhatyigit
+
+Hangman Game with React
+
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+
+## Installation
+
+You can clone the repo first.
+
+```bash
+git clone https://github.com/patika-hepsiburada-react-bootcamp/odev-2-serhatyigit.git
+```
+
+## Usage
+
+After openning the project, inside the root folder plese run the following script to install required packages.
+
+```bash
+yarn install
+```
+
+After installation you can open the project in your browser with the following command.
+
+```bash
+yarn start
+```
+
+## Licence
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Header, Figure, WrongLetters, Word, Notification, Popup } from "./components";
+import { showNotification as show } from "./helpers/helpers";
+
+const words = [
+  "application",
+  "programming",
+  "interface",
+  "wizard",
+  "hepsiburada",
+  "patika",
+  "react",
+  "teacher",
+  "bootcamp",
+  "mac",
+  "windows",
+];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  const playAgain = () => {
+    setPlayable(true);
+
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup
+        correctLetters={correctLetters}
+        wrongLetters={wrongLetters}
+        selectedWord={selectedWord}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
+      <Notification showNotification={showNotification} />
+    </>
+  );
+}
+
+export default App;
